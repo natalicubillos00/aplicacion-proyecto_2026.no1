9fe2220ab27d0767bd531a4d06b4f6b9899686b1
Nicolle Natali Viquez Fonseca- My aplicacion <natalicubillos00@gmail.com>
2026-04-17 12:46:00 -0500

    Proyecto de aplicacion

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -19,7 +19,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-class mainactivity : ComponentActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
